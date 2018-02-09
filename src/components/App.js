0b8c3d3eeb17abbb2031b861342786669c6e08bb
Olivier Monnier <omonnier.dev@gmail.com>
2018-02-09 23:57:20 +0100

    in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions';
 
 class App extends React.Component {
   render() {
     const { props } = this.props;
 
     return (
+      <div>
+
+      </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(states) {
+  return {
+    states
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  actions: bindActionCreators(actions, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/utils/thunkCreator.js
@@ -0,0 +1,19 @@
+export default function thunkCreator(action) {
+  const { types, promise, ...rest } = action;
+  const [ REQUESTED, RESOLVED, REJECTED ] = types;
+
+  return function(dispatch) {
+    dispatch({ ...rest, type: REQUESTED })
+
+    return promise
+      .then(result => {
+        if (result.error) throw new Error(result.error)
+        dispatch({ ...rest, type: RESOLVED, result })
+        return result
+      })
+      .catch(error => {
+        dispatch({ ...rest, type: REJECTED, error })
+        throw error
+      })
+  }
+}
